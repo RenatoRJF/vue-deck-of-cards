105b11813a836b5329218aa03e2ff8c863645578
Renato Ferreira <renatoferreira@MacBook-Pro.domain_not_set.invalid>
2020-06-05 14:35:10 +0200

    Customize OrderedPile component

--- src/views/OrderedPile.vue
+++ src/views/OrderedPile.vue
@@ -1,19 +1,82 @@
 <template>
-  <Container :title="title">
-    <h1>Ordered Pile</h1>
+  <Container :title="'Ordered Pile'">
+    <div class="pile">
+      <Card v-for="(card, index) in cards" :key="index" :index="index" :card="card" />
+    </div>
+
+    <div class="pile__details">
+      <h3>Rotation Card: {{ rotationCard.value }}{{ rotationCard.suit }}</h3>
+
+      <h3>High Card: {{ highestCard }}</h3>
+
+      <div class="full__house-combination">
+        <h3 v-if="fullHouseComb.length === 0">Full House Combo: None</h3>
+        <div v-else>
+          <h3>Full House Combo:</h3>
+
+          <ul class="comnination__list">
+            <li v-for="combination in fullHouseComb" :key="combination">{{ combination }}</li>
+          </ul>
+
+          <p>
+            <strong>{{ fullHouseComb.length }}</strong> combinations
+          </p>
+        </div>
+      </div>
+    </div>
   </Container>
 </template>
 
 <script lang="ts">
 import { Vue, Component } from "vue-property-decorator";
 import Container from "../components/Container.vue";
+import Card from "../components/Card.vue";
+import { Card as CardType } from "../types/card";
 
 @Component({
   components: {
-    Container
+    Container,
+    Card
   }
 })
 export default class OrderedPile extends Vue {
-  private title = "Ordered Pile";
+  private fullHouseComb: string[] = [];
+  private rotationCard = "";
+  private cards: CardType[] = [];
+  private highestCard = "";
 }
 </script>
+
+<style lang="scss" scoped>
+.pile {
+  margin: 20px 50px;
+  background-color: #d8d8d8;
+  border: 1px solid #979797;
+  display: flex;
+  flex-wrap: wrap;
+}
+
+.pile__details {
+  padding: 20px 0 50px 100px;
+
+  h3 {
+    font-size: 25px;
+  }
+}
+
+.comnination__list {
+  width: 100%;
+  display: flex;
+  flex-wrap: wrap;
+  padding: 0;
+
+  li {
+    display: block;
+    background-color: #fff;
+    padding: 5px 10px;
+    margin: 5px;
+    font-weight: bold;
+    color: #545454;
+  }
+}
+</style>
