06e7dfd9ee1babac17381e729b44c4a08235e2a4
Renato Ferreira <renatoferreira@MacBook-Pro.domain_not_set.invalid>
2020-06-05 19:51:04 +0200

    Write a test to check form submission

--- src/components/__tests__/NewDeckForm.spec.ts
+++ src/components/__tests__/NewDeckForm.spec.ts
@@ -1,9 +1,24 @@
-import { shallowMount } from "@vue/test-utils";
+import { shallowMount, mount } from "@vue/test-utils";
 import NewDeckForm from "@/components/NewDeckForm.vue";
 
 describe("NewDeckForm.vue", () => {
   it("renders without crashing", () => {
     const wrapper = shallowMount(NewDeckForm);
     expect(wrapper).toMatchSnapshot();
+  });
+
+  it("emit onSubmit event when form is submitted", async () => {
+    const wrapper = mount(NewDeckForm);
+
+    wrapper.vm.$emit("onSubmit", {});
+
+    wrapper.find("[data-card]").setValue("2S");
+    wrapper.find("[data-rotation-card]").setValue("3H");
+
+    wrapper.find("form").trigger("submit.prevent");
+
+    await wrapper.vm.$nextTick();
+
+    expect(wrapper.emitted("onSubmit")).toBeTruthy();
   });
 });
